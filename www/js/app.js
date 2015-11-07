89ec92eae8d847d0a57673e0e3a792e48a51d006
Ian Luddy <ianluddy@gmail.com>
2015-11-07 16:52:13 +0000

    UI

--- www/js/app.js
+++ www/js/app.js
@@ -27,20 +27,68 @@ angular.module('starter', ['ionic'])
     $scope.modal = modal
   })
 
+  $scope.data = {
+    "volume": 20,
+    "quantity": 6,
+    "strength": 6,
+    "cost": 10,
+  }
+
+  $scope.drinks = [];
+  $scope.drinks_added = false;
+
+  /* Drink Functions */
+
+  $scope.addDrink = function() {
+    $scope.modal.hide();
+    $scope.drinks_added = true;
+    $scope.refreshDrinks($scope.newDrink());
+  };
+
+  $scope.refreshDrinks = function(new_drink){
+      $scope.drinks.push(new_drink);
+      $scope.drinks.sort(function(a,b){return b.index-a.index});
+      console.log($scope.drinks);
+  }
+
+  $scope.newDrink = function(){
+    var new_drink = {
+        volume: $scope.data.volume,
+        strength: $scope.data.strength,
+        quantity: $scope.data.quantity,
+        cost: $scope.data.cost
+    }
+    $scope.evaluateDrink(new_drink);
+    return new_drink;
+  }
+
+  $scope.evaluateDrink = function(drink){
+      drink.mils = $scope.normaliseVolume(drink.unit, drink.volume, drink.quantity);
+      drink.alc = (drink.mils/100) * drink.strength;
+      drink.index = drink.alc/drink.cost;
+  }
+
+  $scope.normaliseVolume = function(unit, volume, quantity){
+      switch(unit){
+          case "l":
+            return volume*1000*quantity;
+          case "cl":
+            return volume*10*quantity;
+          case "oz":
+            return volume*28.4*quantity;
+          default:
+            return volume*quantity;
+      }
+  }
+
+  /* Modal Control */
+
   $scope.openModal = function() {
     $scope.modal.show()
   }
 
   $scope.closeModal = function() {
     $scope.modal.hide();
-  };
-
-  $scope.addDrink = function() {
-
-    $scope.modal.hide();
-
-
-
   };
 
   $scope.$on('$destroy', function() {
